d2c23f2a792fe3f162361ccfb1fe3aa6b248813d
bopes <clcblee@gmail.com>
2016-05-18 13:53:41 -0700

    Create ratin returns json

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,10 +1,11 @@
 class RatingsController < ApplicationController
   def create
-    Rating.create(rater_id: params[:rater_id], ratee_id: params[:ratee_id], score: params[:rating])
+    rating = Rating.create(rater_id: params[:rater_id], ratee_id: params[:ratee_id], score: params[:rating])
     user = User.find(params[:ratee_id])
     sum = user.ratings_received.inject(0) {|total, rating| total += rating.score}
     number_of_ratings = user.ratings_received.count
     new_rating = sum / number_of_ratings
     user.update(rating: new_rating)
+    render json: rating
   end
 end
