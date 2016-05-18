c9cd9e1f37087c902ef044dab16c90d0079e18cf
bopes <clcblee@gmail.com>
2016-05-18 13:51:26 -0700

    Fix create rating bug

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,6 @@
 class RatingsController < ApplicationController
   def create
-    Rating.create(rater_id: params[:rater_id], ratee_id: params[:ratee_id], rating: params[:rating])
+    Rating.create(rater_id: params[:rater_id], ratee_id: params[:ratee_id], score: params[:rating])
     user = User.find(params[:ratee_id])
     sum = user.ratings_received.inject(0) {|total, rating| total += rating.score}
     number_of_ratings = user.ratings_received.count
